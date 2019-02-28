34807b83459e33423e11d371b45a4afcc595f76f
Piotr Pilip <piotr.pilip@gmail.com>
2019-02-28 19:25:05 +0100

    Added reading from file plus parsing

---
+++ iomodule.py
@@ -0,0 +1,12 @@
+def parse(line):
+    data = line.split(' ')
+    return data[0], data[1], data[2:]
+
+
+def read_file(filepath):
+    with open(filepath) as fp:
+        number_of_files = int(fp.readline().strip())
+        for line in fp:
+            yield parse(line.strip())
+
+
